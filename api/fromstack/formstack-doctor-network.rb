b0fa79a1d66fc2b80d9285a6930e76544ef29c6d
david mz <david.mazvosky@gmail.com>
2014-03-09 23:30:33 +0200

    fix type

--- api/fromstack/formstack-doctor-network.rb
+++ api/fromstack/formstack-doctor-network.rb
@@ -28,82 +28,84 @@ end
 class FormStackDoctorNetwork < Grape::API
   format :json
   resource :forms do
-  post '/doctor-network' do
+    post '/doctor-network' do
 
-    logger = Logger.new('biotech-post-log.log')
+      logger = Logger.new('biotech-post-log.log')
 
-    logger.info "Form"
-    logger.info JSON.pretty_generate(params)
+      logger.info "Form"
+      logger.info JSON.pretty_generate(params)
 
-    ship_address = CompositeDecoder.decode params['When we ship the cream to you, someone will have to sign for the package, being that it is a medication. It will be shipped during normal business hours of 8am -5pm. Where would you like to have it shipped to? (Home, Work, etc…., ask if there is A Unit #)']
-
-
-    lead_full_name = CompositeDecoder.decode(params['Name'])
-    doctor_name = params['Ok. Great!! What’s the name of the Doctor you’re seeing for your pain?'].split(' ')
+      ship_address = CompositeDecoder.decode [params['When we ship the cream to you, someone will have to sign for the package, being that it is a medication. It will be shipped during normal business hours of 8am -5pm. Where would you like to have it shipped to? (Home, Work, etc…., ask if there is A Unit #)',
+                                                     params['Shipping Address'],
+                                                     params['Prospects Address']].compact.first
 
 
-    form = {
-        full_name: "#{lead_full_name['first']} #{lead_full_name['last']}",
-        phone: [params['Primary Phone #'], params['Phone']].compact.first,
-        insuranceName: params['What is the name of your Health insurance carrier?'],
-        insurancePlanNumber: params['On the front of the card you should see your Policy Number or Member ID Number. What is that number? '],
-        nsuranceGroupNumber: params['What is the RX Group # Number? '],
-        insuranceBinNumber: params['What is the RX BIN # Number?'],
-        insurancePCNNumber: params['What is the PCN Number?'],
-        address: 'unspecified',
-        address2: 'unspecified',
-        city: 'unspecified',
-        stateCode: 'NY',
-        zipcode: '12345',
-        ship_to_address: ship_address['address'],
-        ship_to_address2: ship_address['address2'],
-        ship_to_city: ship_address['city'],
-        ship_to_stateCode: ship_address['state'],
-        ship_to_zipcode: ship_address['zip'],
-        PhysicianNPI: 'unspecified',
-        PhysicianFirstName: doctor_name[0],
-        PhysicianLastName: (doctor_name[1] rescue 'Not given'),
-        PhysicianAddress1: 'unspecified',
-        PhysicianAddress2: 'unspecified',
-        PhysicianCity: 'unspecified',
-        PhysicianState: 'NY',
-        PhysicianZip: '12345',
-        PhysicianPhone: [params['Do you know the phone number?'],params['Do you know the phone number']].compact.first,
-        PhysicianFax: '9876111111',
-    }
+      lead_full_name = CompositeDecoder.decode(params['Name'])
+      doctor_name = params['Ok. Great!! What’s the name of the Doctor you’re seeing for your pain?'].split(' ')
 
-    if params['What is your DOB?']
-      dob_param = params['What is your DOB?']
-    elsif params['What is your DOB? (Must be 64yrs old and younger)']
-      dob_param = params['What is your DOB? (Must be 64yrs old and younger)']
-    end
-    matched_date = dob_param.scan(/(\d+)\/(\d+)\/(\d+)/)
 
-    if matched_date.length > 0
-      date_of_birth = "#{matched_date[0][2]}-#{matched_date[0][0]}-#{matched_date[0][1]}"
-      form[:dateOfBirth] = date_of_birth
-    end
+      form = {
+          full_name: "#{lead_full_name['first']} #{lead_full_name['last']}",
+          phone: [params['Primary Phone #'], params['Phone']].compact.first,
+          insuranceName: params['What is the name of your Health insurance carrier?'],
+          insurancePlanNumber: params['On the front of the card you should see your Policy Number or Member ID Number. What is that number? '],
+          insuranceGroupNumber: params['What is the RX Group # Number? '],
+          insuranceBinNumber: params['What is the RX BIN # Number?'],
+          insurancePCNNumber: params['What is the PCN Number?'],
+          address: 'unspecified',
+          address2: 'unspecified',
+          city: 'unspecified',
+          stateCode: 'NY',
+          zipcode: '12345',
+          ship_to_address: ship_address['address'],
+          ship_to_address2: ship_address['address2'],
+          ship_to_city: ship_address['city'],
+          ship_to_stateCode: ship_address['state'],
+          ship_to_zipcode: ship_address['zip'],
+          PhysicianNPI: 'unspecified',
+          PhysicianFirstName: doctor_name[0],
+          PhysicianLastName: (doctor_name[1] rescue 'Not given'),
+          PhysicianAddress1: 'unspecified',
+          PhysicianAddress2: 'unspecified',
+          PhysicianCity: 'unspecified',
+          PhysicianState: 'NY',
+          PhysicianZip: '12345',
+          PhysicianPhone: [params['Do you know the phone number?'], params['Do you know the phone number']].compact.first,
+          PhysicianFax: '9876111111',
+      }
 
-    login_hash = {
-        controller: 'leads',
-        action: 'importOrders',
-    }
+      if params['What is your DOB?']
+        dob_param = params['What is your DOB?']
+      elsif params['What is your DOB? (Must be 64yrs old and younger)']
+        dob_param = params['What is your DOB? (Must be 64yrs old and younger)']
+      end
+      matched_date = dob_param.scan(/(\d+)\/(\d+)\/(\d+)/)
 
-    center_code = params['code']
+      if matched_date.length > 0
+        date_of_birth = "#{matched_date[0][2]}-#{matched_date[0][0]}-#{matched_date[0][1]}"
+        form[:dateOfBirth] = date_of_birth
+      end
 
-    login_details = $settings[:centers].select { |x| center_code.downcase == x[:signature].to_s.downcase }.first
+      login_hash = {
+          controller: 'leads',
+          action: 'importOrders',
+      }
 
-    login_hash[:email] = login_details[:login]
-    login_hash[:password] = login_details[:password]
-    subdomain = login_details[:subdomain]
+      center_code = params['code']
 
-    response = RestClient.post "https://#{subdomain}.insuracrm.com/api/", form.merge(login_hash)
+      login_details = $settings[:centers].select { |x| center_code.downcase == x[:signature].to_s.downcase }.first
 
-    logger.info 'Submitted'
-    logger.info JSON.pretty_generate(form)
-        logger.info "Response of post: #{response.body}"
+      login_hash[:email] = login_details[:login]
+      login_hash[:password] = login_details[:password]
+      subdomain = login_details[:subdomain]
 
-    'OK'
+      response = RestClient.post "https://#{subdomain}.insuracrm.com/api/", form.merge(login_hash)
+
+      logger.info 'Submitted'
+      logger.info JSON.pretty_generate(form)
+      logger.info "Response of post: #{response.body}"
+
+      'OK'
     end
   end
 end
