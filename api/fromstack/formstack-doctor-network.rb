f6f3ba135498810663d1ac7e3d99b5c931ac86e1
david mz <david.mazvosky@gmail.com>
2014-03-10 18:28:01 +0200

    more fixes

--- api/fromstack/formstack-doctor-network.rb
+++ api/fromstack/formstack-doctor-network.rb
@@ -46,7 +46,7 @@ class FormStackDoctorNetwork < Grape::API
           full_name: "#{lead_full_name['first']} #{lead_full_name['last']}",
           phone: [params['Primary Phone #'], params['Phone']].compact.first,
           insuranceCarrierName: [params['What is the name of your Health insurance carrier?'], params['Insurance Company Name']].compact.first,
-          insuranceCarrierPhone: [params['What’s the Phone Number of your Insurance Company?'],params['Insurance Company Phone #']].compact.first,
+          insuranceCarrierPhone: [params['What’s the Phone Number of your Insurance Company?'], params['Insurance Company Phone #']].compact.first,
           insurancePlanNumber: [params['On the front of the card you should see your Policy Number or Member ID Number. What is that number? '], params['Insurance Company Member ID/Policy #']].compact.first,
           insuranceGroupNumber: [params['What is the RX Group # Number? '], params['Insurance RX Group #']].compact.first,
           insuranceBinNumber: [params['What is the RX BIN # Number?'], params['Insurance RX BIN #']].compact.first,
@@ -76,7 +76,7 @@ class FormStackDoctorNetwork < Grape::API
         form = form.merge PhysicianFirstName: doctor_name['first'],
                           PhysicianLastName: doctor_name['last']
 
-      else
+      elsif params['Ok. Great!! What’s the name of the Doctor you’re seeing for your pain?']
         doctor_name = params['Ok. Great!! What’s the name of the Doctor you’re seeing for your pain?'].split(' ')
         form = form.merge PhysicianFirstName: doctor_name[0], PhysicianLastName: (doctor_name[1] rescue 'Not given')
       end
