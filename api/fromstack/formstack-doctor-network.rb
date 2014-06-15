7bce2f872c2b017fd55125d489c9b6cd009bbec0
David Mz <david.mazvovsky@gmail.com>
2014-06-15 16:47:26 -0400

    add google doc

--- api/fromstack/formstack-doctor-network.rb
+++ api/fromstack/formstack-doctor-network.rb
@@ -17,7 +17,7 @@ class CompositeDecoder
     }]
   end
 end
- 
+
 class CheckboxListDecoder
   def self.checked?(field, value)
     field.downcase.include?(value.downcase) ? 'Yes' : nil
@@ -35,33 +35,33 @@ class FormStackDoctorNetwork < Grape::API
 
       ship_address = CompositeDecoder.decode [params['When we ship the cream to you, someone will have to sign for the package, being that it is a medication. It will be shipped during normal business hours of 8am -5pm. Where would you like to have it shipped to? (Home, Work, etc…., ask if there is A Unit #)'],
                                               params['Prospects Address'],
-					      params['Please provide me your Physical Address '],
+                                              params['Please provide me your Physical Address '],
                                               params['Shipping Address'],
-					      params['Where would you like to have it shipped to? '],
-					      params['What is your address?'],
-					      params['When we ship the cream to you, someone will have to sign for the package, being that it is a medication. It will be shipped during normal business hours of 8am -5pm. What is your physical address?']
-					      ].compact.select { |x| x.length > 0 }.first
+                                              params['Where would you like to have it shipped to? '],
+                                              params['What is your address?'],
+                                              params['When we ship the cream to you, someone will have to sign for the package, being that it is a medication. It will be shipped during normal business hours of 8am -5pm. What is your physical address?']
+                                             ].compact.select { |x| x.length > 0 }.first
 
 
-lead_name_field =  ([params['Name'],params['Now just to confirm, I have your Name spelled as (Confirm First/Last Name)'],params['What is your name as it appears on the card?']].compact.first)
+      lead_name_field = ([params['Name'], params['Now just to confirm, I have your Name spelled as (Confirm First/Last Name)'], params['What is your name as it appears on the card?']].compact.first)
 
-if lead_name_field.include? "\n"
-      lead_full_name = CompositeDecoder.decode lead_name_field
-else
-lead_full_name = {
-first: lead_name_field.split(' ').first,
-last: (lead_name_field.split(' ').last rescue "Not set")
-}
-end
+      if lead_name_field.include? "\n"
+        lead_full_name = CompositeDecoder.decode lead_name_field
+      else
+        lead_full_name = {
+            first: lead_name_field.split(' ').first,
+            last: (lead_name_field.split(' ').last rescue "Not set")
+        }
+      end
       form = {
           full_name: "#{lead_full_name['first']} #{lead_full_name['last']}",
-          phone: [params['Primary Phone #'], params['Phone'],params['What is the best number to reach you?']].compact.first,
-          insuranceCarrierName: [params['What is the name of your Health insurance carrier?'], params['Insurance Company Name'],params['What is the name of your Insurance Company Name?']].compact.first,
-          insuranceCarrierPhone: [params['What’s the Phone Number of your Insurance Company?'], params['Insurance Company Phone #'],params['What is your Insurance Company\'s Phone #?']].compact.first,
-          insurancePlanNumber: [params['On the front of the card you should see your Policy Number or Member ID Number. What is that number? '], params['Insurance Company Member ID/Policy #'],params['What is your Insurance Company\'s Member ID/Policy #']].compact.first,
-          insuranceGroupNumber: [params['What is the RX Group # Number? '], params['Insurance RX Group #'],params['Your RX Group # ?']].compact.first,
-          insuranceBinNumber: [params['What is the RX BIN # Number?'], params['Insurance RX BIN #'],params['Your Insurance RX BIN # ?']].compact.first,
-          insurancePCNNumber: [params['What is the PCN Number?'], params['Insurance PCN Number'],params['What is your PCN Number ?']].compact.first,
+          phone: [params['Primary Phone #'], params['Phone'], params['What is the best number to reach you?']].compact.first,
+          insuranceCarrierName: [params['What is the name of your Health insurance carrier?'], params['Insurance Company Name'], params['What is the name of your Insurance Company Name?']].compact.first,
+          insuranceCarrierPhone: [params['What’s the Phone Number of your Insurance Company?'], params['Insurance Company Phone #'], params['What is your Insurance Company\'s Phone #?']].compact.first,
+          insurancePlanNumber: [params['On the front of the card you should see your Policy Number or Member ID Number. What is that number? '], params['Insurance Company Member ID/Policy #'], params['What is your Insurance Company\'s Member ID/Policy #']].compact.first,
+          insuranceGroupNumber: [params['What is the RX Group # Number? '], params['Insurance RX Group #'], params['Your RX Group # ?']].compact.first,
+          insuranceBinNumber: [params['What is the RX BIN # Number?'], params['Insurance RX BIN #'], params['Your Insurance RX BIN # ?']].compact.first,
+          insurancePCNNumber: [params['What is the PCN Number?'], params['Insurance PCN Number'], params['What is your PCN Number ?']].compact.first,
           address: 'unspecified',
           address2: 'unspecified',
           city: 'unspecified',
@@ -92,7 +92,7 @@ end
         form = form.merge PhysicianFirstName: doctor_name[0], PhysicianLastName: (doctor_name[1] rescue 'Not given')
       end
 
-      dob_param = [params['What is your DOB? (Must be 64yrs old and younger)'], params['What is your DOB? (Must be 64yrs old and younger)'], params['Date Of Birth'],params['What is your date of birth?'], params['What is your DOB?'],params['What is your DOB? '],params['What is your Date Of Birth?']].compact.first
+      dob_param = [params['What is your DOB? (Must be 64yrs old and younger)'], params['What is your DOB? (Must be 64yrs old and younger)'], params['Date Of Birth'], params['What is your date of birth?'], params['What is your DOB?'], params['What is your DOB? '], params['What is your Date Of Birth?']].compact.first
       matched_date = dob_param.scan(/(\d+)\/(\d+)\/(\d+)/)
 
       if matched_date.length > 0
@@ -112,6 +112,44 @@ end
       login_hash[:email] = login_details[:login]
       login_hash[:password] = login_details[:password]
       subdomain = login_details[:subdomain]
+
+      session = GoogleDrive.login($settings[:google][:username],$settings[:google][:password] )
+      ws = session.spreadsheet_by_key($settings[:google][:doc_id]).worksheets[0]
+
+      new_row_num = ws.rows.length + 1
+
+      ws[new_row_num, 1] = Time.now.to_s
+      ws[new_row_num, 2] = params['dateOfBirth']
+      ws[new_row_num, 3] = params['full_name']
+
+      ws[new_row_num, 4] = params['phone']
+
+      ws[new_row_num, 5] = params['insuranceCarrierName']
+
+      ws[new_row_num, 6] = params['insuranceCarrierPhone']
+      ws[new_row_num, 7] = params['insuranceName']
+      ws[new_row_num, 8] = params['insurancePlanNumber']
+      ws[new_row_num, 9] = params['insuranceGroupNumber']
+      ws[new_row_num, 10] = params['insuranceBinNumber']
+      ws[new_row_num, 11] = params['insurancePCNNumber']
+      ws[new_row_num, 12] = params['address']
+      ws[new_row_num, 12] = params['city']
+      ws[new_row_num, 12] = params['stateCode']
+      ws[new_row_num, 12] = params['zipcode']
+      ws[new_row_num, 12] = params['ship_to_address']
+      ws[new_row_num, 12] = params['ship_to_city']
+      ws[new_row_num, 12] = params['ship_to_stateCode']
+      ws[new_row_num, 12] = params['PhysicianNPI']
+      ws[new_row_num, 12] = params['PhysicianFirstName']
+      ws[new_row_num, 12] = params['PhysicianLastName']
+      ws[new_row_num, 12] = params['PhysicianAddress1']
+      ws[new_row_num, 12] = params['PhysicianCity']
+      ws[new_row_num, 12] = params['PhysicianState']
+      ws[new_row_num, 12] = params['PhysicianZip']
+      ws[new_row_num, 12] = params['PhysicianPhone']
+      ws[new_row_num, 12] = params['PhysicianFax']
+
+      ws.save
 
       response = RestClient.post "https://#{subdomain}.insuracrm.com/api/", form.merge(login_hash)
 
